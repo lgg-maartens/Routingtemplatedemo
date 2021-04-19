2c012d9f5557a20fdb4eb8c55e03340aeda7edf9
Tim Logtenberg <tplogtenberg@gmail.com>
2021-04-19 12:52:19 +0200

    Retrieve names from database

--- Routingtemplatedemo/Controllers/HomeController.cs
+++ Routingtemplatedemo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using Routingtemplatedemo.Models;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,9 @@ namespace Routingtemplatedemo.Controllers
 
     public IActionResult Index()
     {
-      return View();
+      var names = GetNames();
+      
+      return View(names);
     }
 
     public IActionResult Privacy()
@@ -40,5 +43,42 @@ namespace Routingtemplatedemo.Controllers
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    public List<string> GetNames()
+    {
+      // stel in waar de database gevonden kan worden
+      string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=;";
+
+      // maak een lege lijst waar we de namen in gaan opslaan
+      List<string> names = new List<string>();
+
+      // verbinding maken met de database
+      using (MySqlConnection conn = new MySqlConnection(connectionString))
+      {
+        // verbinding openen
+        conn.Open();
+
+        // SQL query die we willen uitvoeren
+        MySqlCommand cmd = new MySqlCommand("select * from product where id = 1", conn);
+
+        // resultaat van de query lezen
+        using (var reader = cmd.ExecuteReader())
+        {
+          // elke keer een regel (of eigenlijk: database rij) lezen
+          while (reader.Read())
+          {
+            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+            string Name = reader["naam"].ToString();
+
+            // voeg de naam toe aan de lijst met namen
+            names.Add(Name);
+          }
+        }
+      }
+
+      // return de lijst met namen
+      return names;
+    }
+
   }
 }
